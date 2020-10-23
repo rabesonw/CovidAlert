d08c4a787df9fd84e519a8b8c90325b06694e576
emma_cbrt <s10600@beuth-hochschule.de>
2020-10-23 10:25:11 +0200

    Création du projet, création du model User

---
+++ src/main/java/alert/covid/models/User.java
@@ -0,0 +1,83 @@
+package alert.covid.models;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity(name="users")
+@Access(AccessType.FIELD)
+public class User{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long user_id;
+
+    private String first_name;
+    private String last_name;
+    private String email;
+    private String phone_number;
+    private String password;
+    private String state;
+
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+
+    public long getUser_id() {
+        return user_id;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhone_number() {
+        return phone_number;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+
+
+    public void setUser_id(long user_id) {
+        this.user_id = user_id;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPhone_number(String phone_number) {
+        this.phone_number = phone_number;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+
+}
